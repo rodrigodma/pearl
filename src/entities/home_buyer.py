c77780c3cd2d36795182c84e82e442ad69d315ee
rodrigodma-yolo <rodrigo.melo@yolocoliving.com.br>
2024-09-03 23:11:17 -0300

    (refact): cleaning code, unused methods

--- src/entities/home_buyer.py
+++ src/entities/home_buyer.py
@@ -4,7 +4,6 @@ class Home_Buyers():
   name: str
   goals: Vector
   preferences: [] # type: ignore
-  current_preference: int
   setted = bool
 
   def __init__(self, name: str, goals: Vector, preferences: list):
@@ -15,10 +14,6 @@ class Home_Buyers():
 
   def fit(self, score: Vector):
     return self.goals.energy_efficiency * score.energy_efficiency + self.goals.water * score.water + self.goals.resilience * score.resilience
-
-  def change_preferences(self):
-    if self.current_preference < len(self.preferences) - 1:
-      self.current_preference += 1
 
   def __str__(self):
     return f'Home_Buyers(goals:{self.goals},preferences:{self.preferences})'
